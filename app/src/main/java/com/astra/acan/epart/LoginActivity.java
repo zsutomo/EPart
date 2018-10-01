fdc94f54a6598ddb9e685a05315c34dab615ee9e
zsutomo <zakariasutomo@gmail.com>
2018-10-01 11:01:36 -0700

    jeck_add menu

--- app/src/main/java/com/astra/acan/epart/LoginActivity.java
+++ app/src/main/java/com/astra/acan/epart/LoginActivity.java
@@ -1,18 +1,85 @@
 package com.astra.acan.epart;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edit_username;
+    private EditText edit_password;
+    private Button btn_Login;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edit_username = (EditText) findViewById(R.id.edittxt_userId);
+        edit_password = (EditText) findViewById(R.id.edittxt_userPass);
+        btn_Login = (Button) findViewById(R.id.btn_login);
+
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, Home.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
 
     public void login(View view) {
 
+        final String username = edit_username.getText().toString();
+        final String password = edit_password.getText().toString();
+
+        auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Salah : " + username + password, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            auth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+
+
 }
